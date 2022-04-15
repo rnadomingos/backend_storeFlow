d583b9819cf5f997a83646f3600fc35e7120a5b7
Renan <rna.domingos@hotmail.com>
2022-04-15 16:03:20 -0300

    feat: Implemented the method List All Segments

--- backend/src/shared/infra/http/routes/segmentRoutes.ts
+++ backend/src/shared/infra/http/routes/segmentRoutes.ts
@@ -1,13 +1,16 @@
 import { CreateSegmentController } from "@modules/segment/useCase/createSegment/CreateSegmentController";
 import { FindByNameSegmentController } from "@modules/segment/useCase/findByNameSegment/FindByNameSegmentController";
+import { ListSegmentController } from "@modules/segment/useCase/listSegment/ListSegmentController";
 import { Router } from "express";
 
 const segmentRoutes = Router();
 
 const createSegmentController = new CreateSegmentController();
 const findBySegmentController = new FindByNameSegmentController();
+const listSegmentController = new ListSegmentController();
 
 segmentRoutes.post("/new", createSegmentController.handle);
 segmentRoutes.get("/get-segment/:name", findBySegmentController.handle);
+segmentRoutes.get("/list", listSegmentController.handle);
 
 export { segmentRoutes }
