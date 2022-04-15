0eb78e6d12fdd9480014f6a5cc60f249d761dc82
Renan <rna.domingos@hotmail.com>
2022-04-15 01:08:12 -0300

    chore: async-error implemented. Finished!

--- backend/src/shared/infra/http/app.ts
+++ backend/src/shared/infra/http/app.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata";
 import "../../container"
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import { router } from "./routes";
 import createConnection from "../typeorm";
@@ -12,5 +12,17 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        return res.status(400).json({
+            message: err.message
+        })
+    }
+    return res.status(500).json({
+        status: 'error',
+        message: "Inernal server error"
+    })
+})
 
 export { app };
