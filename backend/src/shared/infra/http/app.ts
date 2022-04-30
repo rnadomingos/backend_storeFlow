7660ae54dbcd7a02646b23a70479594e79a89006
raulrocha2 <raul.rocha2@gmail.com>
2022-04-30 17:19:41 -0300

    feat: swagger UI create new store with bearerAuth

--- backend/src/shared/infra/http/app.ts
+++ backend/src/shared/infra/http/app.ts
@@ -6,6 +6,7 @@ import YAML from 'yamljs'
 import swaggerUi from 'swagger-ui-express';
 import express, { NextFunction, Request, Response } from "express";
 import { router } from "./routes";
+import cors from "cors";
 import createConnection from "../typeorm";
 import cookieParser from "cookie-parser";
 import { ErrorHandler } from "@shared/errors/ErrorHandler";
@@ -23,7 +24,8 @@ app.use(cookieParser());
 const swaggerPath = join(__dirname, '../../../../swagger.YAML')
 const swaggerFile = YAML.load(swaggerPath)
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use(cors());
 app.use(router);
 
 
