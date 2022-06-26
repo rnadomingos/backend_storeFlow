34ebb9fc6ff11c34f71e2751d5f48bb443b6d2bf
raulrocha2 <raul.rocha2@gmail.com>
2022-06-26 17:53:45 -0300

    feat: send e-mail by gmail

---
+++ backend/src/shared/container/providers/mail/implementations/GmailProvider.ts
@@ -0,0 +1,45 @@
+import { injectable } from "tsyringe"
+import { IMailProvider } from "../IMailProvider"
+import nodemailer, { Transporter } from "nodemailer"
+import Handlebars from "handlebars";
+import fs from "fs";
+
+@injectable()
+export class GmailProvider implements IMailProvider {
+
+  private client: Transporter
+
+  constructor() {
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASS
+      },
+    });
+    this.client = transporter;
+  }
+
+  async sendMail(
+    to: string,
+    subject: string,
+    variables: any,
+    path: string
+  ): Promise<void> {
+
+    const templateFileContent = fs.readFileSync(path).toString("utf-8");
+    const templateParse = Handlebars.compile(templateFileContent);
+
+    const templateHTML = templateParse(variables);
+
+    await this.client.sendMail({
+      to,
+      from: "GB <noreplay@gb.com.br>",
+      subject,
+      html: templateHTML
+    })
+
+  }
+}
