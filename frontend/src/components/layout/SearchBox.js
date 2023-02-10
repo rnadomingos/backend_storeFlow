26aed4975f3f2aa01229f7394d6f8f1fe5cecd16
renan <rna.domingos@hotmail.com>
2023-02-09 23:40:13 -0300

    feat: add style in SearchBox

--- frontend/src/components/layout/SearchBox.js
+++ frontend/src/components/layout/SearchBox.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Button, Col, Form } from 'react-bootstrap'
 import { useHistory } from 'react-router-dom'
+import '../../css/searchBox.css';
 
 function SearchBox({url}) {
     const [keyword, setKeyword] = useState('')
@@ -16,24 +17,25 @@ function SearchBox({url}) {
         }
     }
     return (
-        
-        <Form onSubmit={submitHandler} className="input-group">
-            <Col sm="5">
-                <Form.Control
-                    type='text'
-                    name='q'
-                    onChange={(e) => setKeyword(e.target.value)}
-                    className='form-control mr-sm-2'
-                ></Form.Control>
-            </Col>
-            <Button
-                type='submit'
-                variant='secondary'
-                className='btn btn-secondary'
-            >
-                <i className="fas fa-search"></i>
-            </Button>
-        </Form>
+        <div className='searchBox'>
+            <Form onSubmit={submitHandler} className="input-group">
+                <Col sm="5">
+                    <Form.Control
+                        type='text'
+                        name='q'
+                        onChange={(e) => setKeyword(e.target.value)}
+                        className='form-control mr-sm-2'
+                    ></Form.Control>
+                </Col>
+                <Button
+                    type='submit'
+                    variant='secondary'
+                    className='btn btn-secondary'
+                >
+                    <i className="fas fa-search"></i>
+                </Button>
+            </Form>
+        </div>
     )
 }
 
